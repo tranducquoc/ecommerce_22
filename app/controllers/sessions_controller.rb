541ed2621a33e242e6f4f92dd8dcdc74b35689cb
Thanh Luan <bo.panda.qt@gmail.com>
2017-03-02 10:12:21 +0700

    signup logout login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def admin_login
+    admin = User.find_by(email: params[:session][:email].downcase)
+    if admin && admin.authenticate(params[:session][:password]) && admin.is_admin?
+      login_admin admin
+      redirect_to admin_path
+    else
+      flash.now[:danger] = t "invalid_email_password_combination"
+      render :new
+    end
+  end
+
+  def admin_logout
+    logout_admin
+    redirect_to admin_path
+  end
+
+  def login
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to root_path
+    else
+      flash.now[:danger] = t "invalid_email_password_combination"
+      render :new
+    end
+  end
+
+  def logout
+    log_out if loged?
+    redirect_to root_url
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,62 @@
+module SessionsHelper
+  def loged_admin?
+    session[:admin_id].present?
+  end
+
+  def login_admin admin
+    session[:admin_id] = admin.id
+  end
+
+  def logout_admin
+    session.delete(:admin_id)
+  end
+
+  def log_inuser
+   session[:user_id] = user.id
+  end
+
+  def log_out
+    forget current_user
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def loged?
+    current_user.present?
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  def forget user
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget current_user
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+end
